4e0265493433288a94d6eafef3ae3e5155daf596
Drozzzd <42914695+Drozzzd@users.noreply.github.com>
2025-01-23 09:48:24 +0300

    Initial commit

---
+++ test_app.py
@@ -0,0 +1,19 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+	with app.test_client() as client:
+		yield client
+
+# Тест для маршрута, возвращающего данные
+def test_data_page(client):
+	response = client.get('/data')
+	assert response.status_code == 200
+	assert b'This is some data!' in response.data
+
+ # Тест для проверки главной страницы
+def test_home_page(client):
+	response = client.get('/')
+	assert response.status_code == 200
+	assert b'Welcome' in response.data
